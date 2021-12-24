4e33fb9f19d3958c7dbf131923a70ef313595f8e
Shima-Oh <shimasound04@gmail.com>
2021-12-25 02:44:37 +0900

    feat(header.tsx): sign in github

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,15 +1,27 @@
-import { Flex, Button, Heading, Stack } from '@chakra-ui/react';
+import { Flex, Button, Heading, Stack, Text, Box } from '@chakra-ui/react';
+import { signIn, signOut, useSession } from 'next-auth/react';
 
 const Header = () => {
+  const { data: session } = useSession();
+
   return (
     <Flex as='header' p={4} justifyContent='space-between'>
       <Heading size='lg'>Todo</Heading>
-      <Stack as='nav' direction='row' spacing={4}>
-        <Button colorScheme='teal' variant='outline' px={6}>
-          Sign In
+      {!session ? (
+        <Button
+          colorScheme='teal'
+          variant='outline'
+          px={6}
+          onClick={() => signIn('github', { callbackUrl: '/' })}
+        >
+          Sign In with GitHub
         </Button>
-        <Button colorScheme='teal'>create account</Button>
-      </Stack>
+      ) : (
+        <Stack as='nav' direction='row' spacing={4} alignItems='center'>
+          <Text>{session.user?.name}</Text>
+          <Button onClick={() => signOut()}>Sign Out</Button>
+        </Stack>
+      )}
     </Flex>
   );
 };
